8547174d3df53921fd4089c043ec0cca0b3ccaf8
Abdurahmono4 <olimjonovrahmonjon7@gmail.com>
2024-01-15 01:36:58 +0500

    new solutions for teacher matters

---
+++ index.js
@@ -0,0 +1,96 @@
+/**
+ * 1-masala:
+ * let musbat = prompt("please enter your favourite number");
+    if (musbat > 0 ) {
+    document.write("your number is positive!");
+      } else if (musbat < 0) {
+     document.write("your number is negative!");
+   
+    }
+ * 
+    2-masala:
+     let musbat = prompt("please enter your favourite number");
+     if (musbat < 100 && musbat > 89) {
+       document.write("your number is between! 89-100");
+     } else if (musbat < 89 && musbat > 80) {
+       document.write("your number is between! 89-80");
+     } else if (musbat < 80 && musbat > 69) {
+       document.write("your number is between! 70-79");
+     } else if (musbat < 70 && musbat > 59) {
+       document.write("your number is between! 69-60");
+     } else if (musbat < 60 && musbat > 0) {
+       document.write("your number is between! 59-0");
+     }
+ * 
+     3-masala: 
+
+ *    let number1 = +prompt("please enter year ");
+
+    if (number1 % 4 == 0) {
+   document.write("Siz kiritgan yil kabisa yili");
+   console.log("Siz kiritgan yil kabisa yili");
+  }  else {
+   document.write("Siz kiritgan yil kasbisa yili emas ");
+   console.log("Siz kiritgan yil kabisa yili emas");
+   }
+ 
+     4-masala:
+ *  let musbat = prompt("please enter your favourite number");
+    if (musbat > 0 &&  musbat != 0) {
+    document.write("your number is positive!");
+      } else if (musbat < 0 && musbat != 0) {
+     document.write("your number is negative!");
+    }
+
+    if (musbat=0){
+        document.write("your number is zero!")
+    }
+
+     5-masala:
+     let number1 = +prompt("please enter first number ");
+        let number2 = +prompt("please enter second number");
+         if (number1 > number2) {
+        document.write("Birinchi son ikkinchisidan katta");
+        } else if (number1 < number2) {
+          document.write("Ikkinchi son birinchi sondan katta");
+    }
+ *   6-masala:
+          let number1 = +prompt("please enter first number ");
+      let number2 = +prompt("please enter second number");
+      let number3 = +prompt("please enter third number ");
+      if (number1 < number2 && number1 < number3) {
+        document.write("Birinchi son eng kichik son");
+      } else if (number1 > number2 && number2 < number3) {
+        document.write("Ikkinchi son eng kichigi");
+      } else if (number3 < number1 && number2 > number3) {
+        document.write("Uchinchi son eng kichik son");
+      }
+
+      7-masala;
+              let number1 = +prompt("please enter first number ");
+        let number2 = +prompt("please enter second number ");
+        let number3 = +prompt("please enter third number ");
+
+        let manfiyYigindi = 0;
+        if (number1 < 0) {
+          manfiyYigindi += number1;
+        }
+        if (number2 < 0) {
+          manfiyYigindi += number2;
+        }
+        if (number3 < 0) {
+          manfiyYigindi += number3;
+        }
+
+        console.log(manfiyYigindi);
+
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
